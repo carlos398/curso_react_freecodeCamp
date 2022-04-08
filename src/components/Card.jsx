237bda36b36a60ce23ce80fb14fd8f5c57d04866
carlos reyes <carlosarp2000@gmail.com>
2022-04-07 21:08:39 -0500

    creacion de las cards component

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <section>
+            <img src="" alt="" />
+        </section>
+        <section>
+            <h2>title</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur vel tempora magnam veniam beatae. Dolores numquam quod doloremque, vitae culpa inventore eligendi at maiores ex ab dicta? Beatae quam minima vitae placeat nam fugit iste sequi assumenda fuga omnis, saepe nostrum repudiandae reiciendis magni accusantium, ea perspiciatis sed, asperiores optio culpa tenetur a eum voluptatem labore. Tempora, et in, enim eligendi molestiae, ducimus quis at commodi id saepe labore culpa. Culpa, numquam porro. Ea aspernatur dolorem iste saepe quasi officia dignissimos, unde sint nemo sunt reiciendis repellendus assumenda est possimus quaerat, commodi aut tempora explicabo, corporis obcaecati culpa? Impedit quam iusto officia adipisci nesciunt! Facilis doloribus eaque deserunt rerum voluptate. Totam, adipisci atque commodi quasi quos incidunt? Maiores quis, placeat amet repudiandae inventore similique obcaecati harum. Odit veritatis natus odio, aliquid sit officia eum ratione corporis velit optio, fugiat eos ea dolores voluptatibus, tenetur deserunt. Consectetur, inventore, molestiae commodi dolor, fugiat debitis excepturi impedit quos non voluptatibus quia! Modi dolorem quisquam facilis doloremque excepturi earum? Soluta earum eius debitis quidem architecto, error saepe, quo voluptate libero quaerat, amet ullam commodi reiciendis fugiat perspiciatis culpa fuga atque! Velit illum dolor sapiente voluptate blanditiis! Ea ratione atque placeat earum et ut illum!</p>
+        </section>
+    </div>
+  )
+}
+
+export default Card
