c403f1d86836e4e3578bb9fd92c537cea8f869ef
carlos reyes <carlosarp2000@gmail.com>
2022-04-09 18:40:25 -0500

    creacion visual de las targetas de projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Card from "./components/Card";
+import "./App.css"
 
 
 function App() {
   return (
     <div className="App">
+      <h1>TESTIMONIOS ESTUDIANTES FREE CODE CAMP</h1>
       <Card/>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import "./Card.css"
 
 const Card = () => {
   return (
-    <div>
-        <section>
-            <img src="" alt="" />
+    <div className='contenedor-card'>
+        <section className='contenedor-imagen-testimonio'>
+            <img className='imagen-testimonio' src="" alt="" />
         </section>
-        <section>
-            <h2>title</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur vel tempora magnam veniam beatae. Dolores numquam quod doloremque, vitae culpa inventore eligendi at maiores ex ab dicta? Beatae quam minima vitae placeat nam fugit iste sequi assumenda fuga omnis, saepe nostrum repudiandae reiciendis magni accusantium, ea perspiciatis sed, asperiores optio culpa tenetur a eum voluptatem labore. Tempora, et in, enim eligendi molestiae, ducimus quis at commodi id saepe labore culpa. Culpa, numquam porro. Ea aspernatur dolorem iste saepe quasi officia dignissimos, unde sint nemo sunt reiciendis repellendus assumenda est possimus quaerat, commodi aut tempora explicabo, corporis obcaecati culpa? Impedit quam iusto officia adipisci nesciunt! Facilis doloribus eaque deserunt rerum voluptate. Totam, adipisci atque commodi quasi quos incidunt? Maiores quis, placeat amet repudiandae inventore similique obcaecati harum. Odit veritatis natus odio, aliquid sit officia eum ratione corporis velit optio, fugiat eos ea dolores voluptatibus, tenetur deserunt. Consectetur, inventore, molestiae commodi dolor, fugiat debitis excepturi impedit quos non voluptatibus quia! Modi dolorem quisquam facilis doloremque excepturi earum? Soluta earum eius debitis quidem architecto, error saepe, quo voluptate libero quaerat, amet ullam commodi reiciendis fugiat perspiciatis culpa fuga atque! Velit illum dolor sapiente voluptate blanditiis! Ea ratione atque placeat earum et ut illum!</p>
+        <section className='contenedor-textos-testimonio'>
+            <h2 className='nombre-persona'>title</h2>
+            <h4 className='titulo-persona'>ingeniero-bachiller-etx</h4>
+            <p className='texto-testimonio'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur vel tempora magnam veniam beatae. Dolores numquam quod doloremque, vitae culpa inventore eligendi at maiores ex ab dicta? Beatae quam minima vitae placeat nam fugit iste sequi assumenda fuga omnis, saepe nostrum repudiandae reiciendis magni accusantium, ea perspiciatis sed, asperiores optio culpa tenetur a eum voluptatem labore.</p>
         </section>
     </div>
   )
